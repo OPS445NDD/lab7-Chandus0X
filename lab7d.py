5d84b95290f6b0c7ec9942b612cc0334e4e7bf4e
bchand1 <bchand1@myseneca.ca>
2026-07-10 16:17:06 -0400

    Added lab7d

--- lab7d.py
+++ lab7d.py
@@ -19,10 +19,9 @@ class Time:
 
     def sum_times(self, t2):
         """Add two time objests and return the sum."""
-        self_sec = self.time_to_sec()
-        t2_sec = t2.time_to_sec()
-        sum = sec_to_time(self_sec + t2_sec)
-        return sum
+        total_seconds = self.time_to_sec() + t2.time_to_sec()
+
+        return sec_to_time(total_seconds)
 
     def change_time(self, seconds):
         time_seconds = self.time_to_sec()
